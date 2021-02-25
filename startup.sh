b6924e787fb34195be0dc39ddc22db0d83f35ba4
Leonardo Lamas <leoslamas@gmail.com>
2021-02-24 21:15:38 -0300

    fixing startup script

--- startup.sh
+++ startup.sh
@@ -4,15 +4,15 @@
 TZ='America/Sao_Paulo'
 export TZ
 
-echo "Uploading backup to S3..."
+echo "Uploading World backup to S3..."
 
 # backup world
 zip -r worlds.zip /home/steam/.config/unity3d/IronGate/Valheim/worlds/
 aws s3api put-object --body "worlds.zip" --bucket "amnesicbucket" --key "valheim/backups/worlds-$(date +%d_%m_%y-%H_%M).zip" --acl "public-read" --content-type "application/zip"
 rm worlds.zip
 
+echo "Starting Valheim server..."
+
 # start server
-sudo -iu steam
-cd /home/steam/valheim
-rm nohup.out
-nohup ./start_server.sh &
+runuser -l steam -c 'rm -f /home/steam/valheim/nohup.out'
+runuser -l steam -c 'cd /home/steam/valheim; nohup ./start_server.sh &'
