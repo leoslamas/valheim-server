149b5d02632259e524f5013674b2252b15677e58
Leonardo Lamas <leoslamas@gmail.com>
2021-02-25 15:06:35 -0300

    updating server on startup

--- upload_image.sh
+++ upload_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 243101742269.dkr.ecr.us-east-1.amazonaws.com
 docker build -t valheim .
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 243101742269.dkr.ecr.us-east-1.amazonaws.com
 docker tag valheim:latest 243101742269.dkr.ecr.us-east-1.amazonaws.com/valheim:latest
 docker push 243101742269.dkr.ecr.us-east-1.amazonaws.com/valheim:latest
