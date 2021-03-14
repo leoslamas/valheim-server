24698b731158b7aa8963a06711c9283bd921068a
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2021-03-13 23:31:21 -0300

    uploading backup to s3 (maybe)

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -1,18 +1,19 @@
+use crate::utils::fetch_env;
 use clap::ArgMatches;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use log::{debug, error};
-use std::fs::File;
 use std::process::exit;
-use crate::utils::fetch_env;
+use std::{fs::File, io::Read};
 
-use rusoto_core::{self, Region};
-use rusoto_s3::{self, PutObjectRequest, S3Client, StreamingBody};
+use rusoto_core::{self, ByteStream, Region};
+use rusoto_s3::{self, PutObjectRequest, S3Client, S3};
+use tokio;
 
 pub fn invoke(args: &ArgMatches) {
   let input = args.value_of("INPUT_DIR").unwrap();
   let output = args.value_of("OUTPUT_FILE").unwrap();
-  
+
   let backup_to_s3 = fetch_env("BACKUP_TO_S3", "0", false).eq("1");
   let s3_bucket = fetch_env("S3_BUCKET", "amnesicbit", false);
   let s3_key = fetch_env("S3_KEY", "backups/", false);
@@ -36,32 +37,32 @@ pub fn invoke(args: &ArgMatches) {
     }
   };
 
-  let body = StreamingBody::new();
+  if !backup_to_s3 {
+    return;
+  }
+  if let Ok(mut file) = File::open(output) {
+    let mut buf: Vec<u8> = vec![];
+    file.read_to_end(&mut buf).unwrap();
+    let byte_stream = ByteStream::from(buf);
 
-  let client = S3Client::new(Region::SaEast1);
-  let request = PutObjectRequest {
-    body: tar_gz,
-    bucket: s3_bucket,
-    key: s3_key,
-    content_type: Some("application/zip".to_string()),
-    acl: Some("public-read".to_string()),
-    ..Default::default()
-  };
-  
-}
+    let client = S3Client::new(Region::SaEast1);
+    let request = PutObjectRequest {
+      body: Some(byte_stream),
+      bucket: s3_bucket,
+      key: s3_key,
+      content_type: Some("application/zip".to_string()),
+      acl: Some("public-read".to_string()),
+      ..Default::default()
+    };
 
-async fn do_upload() -> Result<(), Box<dyn std::error::Error>> {
-    let s3_client = S3Client::new(Region::UsEast1);
-    let mut file = tokio::fs::File::open("text.txt").await?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).await?;
+    let rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
 
-    let result = s3_client.put_object(PutObjectRequest {
-        bucket: String::from("bucket-name"),
-        key: "text.txt".to_string(),
-        body: Some(StreamingBody::from(buffer)),
-        ..Default::default()
-    }).await?;
-    // do thing with result
-    Ok(())
+    match rt.block_on(client.put_object(request)) {
+        Ok(_) => debug!("Backup file uploaded to S3!"),
+        Err(_) => error!("Failed to upload backup ({}) to S3", output)
+    };
+    
+  } else {
+    error!("Failed to open backup file ({})", output)
+  }
 }
